e349b8e570b7db71c2e660a124491c023a0e57ad
marcgq <79192457+marcgq@users.noreply.github.com>
2021-10-06 20:37:33 +0200

    v1.1

--- EditableBufferedReader/WithoutMVC/EditableBufferedReader.java
+++ EditableBufferedReader/WithoutMVC/EditableBufferedReader.java
@@ -1,22 +1,9 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.Reader;
 
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author marc
- */
-
 public class EditableBufferedReader extends BufferedReader {
-    public static final int BACK = 256, FORW = 257, ERASE = 258, SUPR = 259, HOME = 260, END = 270, INS = 271;
-
+    
     public EditableBufferedReader(Reader in) throws IOException {
         super(in);
 
@@ -33,44 +20,32 @@ public class EditableBufferedReader extends BufferedReader {
     @Override
     public int read() throws IOException {
         int key = super.read();
-        if (key != 27 && key != 8) {
-            System.out.print("\033[2K"); // Erase all line in display
-            System.out.print("\033[1G"); // Cursor at 1st column
+        if (key != Key.ESCAPE && key != Key.BACKSPACE) {
             return key;
-        } else if (key == 8) { // Backspace
-            System.out.print("\033[2K"); // Erase all line in display
-            System.out.print("\033[1G"); // Cursor at 1st column
-            return ERASE;
+        } else if (key == Key.BACKSPACE) { 
+            return Key.ERASE;
         } else {
-            super.read();
-            int key2= super.read();
-
-            switch (key2) {
+            if(super.read()!='[') return (Key.BELL);
+            switch (key=super.read()) {
                 case 'C': // Right arrow
-                    System.out.print("\033[1C"); // Cursor forward
-                    return (FORW);
-
+                    return (Key.FORW);
                 case 'D': // Left arrow
-                    System.out.print("\033[1D"); // Cursor backward
-                    return (BACK);
+                    return (Key.BACK);
                 case '1': // Home
-                    super.read();
-                    return (HOME);
-                case '3': // Delete
-                    System.out.print("\033[2K"); // Erase all line in display
-                    System.out.print("\033[1G"); // Cursor at 1st column
-                    super.read();
-                    return (SUPR);
+                    super.read(); //Flush wave hyphen
+                    return (Key.HOME);
                 case '2':
-                    super.read();
-                    return (INS);
+                    super.read(); //Flush wave hyphen
+                    return (Key.INS);
+                case '3': // Delete
+                    super.read(); //Flush wave hyphen
+                    return (Key.SUPR);
                 case '4': // End
-                    super.read();
-                    return (END);
+                    super.read(); //Flush wave hyphen
+                    return (Key.END);
                 default:
-                    return 0;
+                    return (Key.BELL);
             }
-
         }
     }
 
@@ -79,40 +54,32 @@ public class EditableBufferedReader extends BufferedReader {
         Line line = new Line();
         boolean insertMode = true;
         int key = 0;
-        while (key != 13) {
+        while (key != Key.CR) {
             setRaw();
-            key = read();
-
-            switch (key) {
-                case BACK:
-                case FORW:
-                case HOME:
-                case END:
+            switch (key = read()) {
+                case Key.BACK:
+                case Key.FORW:
+                case Key.HOME:
+                case Key.END:
                     line.modifyIndex(key);
                     System.out.print("\033[" + (line.getIndex() + 1) + "G");
                     break;
-                case INS:
+                case Key.INS:
                     insertMode = !insertMode;
                     break;
-                case 13: // Carriage return
-                    System.out.print(line);
-                    break;
-                case ERASE:
+                case Key.ERASE:
+                case Key.SUPR:
                     line.delete(key);
-                    System.out.print(line);
-                    System.out.print("\033[" + (line.getIndex()) + "G"); // Cursor placement
+                    System.out.print("\033[2K" + "\033[1G" + line + "\033[" + (line.getIndex()+1) + "G"); // Erase line, cursor at begg., print line, place cursor
                     break;
-                case SUPR:
-                    line.delete(key);
-                    System.out.print(line);
-                    System.out.print("\033[" + (line.getIndex() + 1) + "G"); // Cursor placement
+                case Key.BELL:
+                    System.out.print("\077"); //Ring bell
+                case Key.CR: // Carriage return
                     break;
                 default:
                     line.insert((char) key, insertMode);
-                    System.out.print(line);
-                    System.out.print("\033[" + (line.getIndex() + 1) + "G"); // Cursor placement
+                    System.out.print("\033[2K" + "\033[1G" + line + "\033[" + (line.getIndex()+1) + "G"); // Erase line, cursor at begg., print line, place cursor
             }
-
         }
         unsetRaw();
         return line.getString();
