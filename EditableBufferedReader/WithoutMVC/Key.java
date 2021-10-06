d2fa77bb590e6a650fd884bc7f45930d120f7080
marcgq <79192457+marcgq@users.noreply.github.com>
2021-10-06 20:38:41 +0200

    v1

---
+++ EditableBufferedReader/WithoutMVC/Key.java
@@ -0,0 +1,6 @@
+public class Key {
+    public static final int CR=13, ESCAPE=27, BACKSPACE=8, BACK = 256, 
+                            FORW = 257, ERASE = 258, SUPR = 259, HOME = 260, 
+                            END = 270, INS = 271, BELL=7;
+
+}
