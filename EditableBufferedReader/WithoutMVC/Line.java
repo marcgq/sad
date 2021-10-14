0458c291837e26ebbf33ceaec9b553f6b1db37a0
marcgq <79192457+marcgq@users.noreply.github.com>
2021-10-14 21:08:16 +0200

    Actualitzat d'acord als canvis a readLine()

--- EditableBufferedReader/WithoutMVC/Line.java
+++ EditableBufferedReader/WithoutMVC/Line.java
@@ -1,13 +1,15 @@
 public class Line {
     int index;
     String str;
-    
+    boolean insertMode;
+
     public Line() {
         index = 0;
         str = "";
+        insertMode = true;
     }
 
-    public void insert(char car, Boolean insert) {
+    public String insert(char car, Boolean insert) {
         if (insert) {
             String cola = str.substring(index);
             str = str.substring(0, index) + car + cola;
@@ -23,51 +25,58 @@ public class Line {
                 index += 1;
             }
         }
+        return str.substring(index-1);
     }
 
-    public void delete(int key) {
-        if (key == Key.ERASE && index > 0) {
+    public void delete() {
+        if (index > 0) {
             String cola = str.substring(index);
             str = str.substring(0, index - 1) + cola;
             index -= 1;
-        } else if (key == Key.SUPR && index < str.length()) {
+        } else {
+            System.out.print("\007");
+        }
+    }
+
+    public void supress() {
+        if (index < str.length()) {
             String cola = str.substring(index + 1);
             str = str.substring(0, index) + cola;
         } else {
             System.out.print("\007");
         }
-
     }
 
-    public void modifyIndex(int key) {
-        switch (key) {
-            case Key.BACK:
-                if (index > 0) {
-                    index -= 1;
-                }  else {
-                    System.out.print("\007");
-                }
-                break;
-            case Key.FORW:
-                if (index < str.length()) {
-                    index += 1;
-                } else {
-                    System.out.print("\007");
-                }
-                break;
-            case Key.HOME:
-                index = 0;
-                break;
-            case Key.END:
-                index = str.length();
+    public boolean incrementIndex(){
+        if (index < str.length()) {
+            index += 1;
+            return true;
+        } else {
+            System.out.print("\007");
+            return false;
         }
+    }
+
+    public boolean decrementIndex(){
+        if (index > 0) {
+            index -= 1;
+            return true;
+        }  else {
+            System.out.print("\007");
+            return false;
+        }
+    }
+    
+    public void homeIndex(){
+        index=0;
+    }
+
+    public void endIndex(){
+        index=str.length();
     }
 
     public int getIndex() {
         return index;
-    }
-    public String getString(){
-        return str;
     }
 
     public String toString() {
