905ceed61201ba2adc4ac52b86ee10bf03f5e676
marcgq <79192457+marcgq@users.noreply.github.com>
2021-10-14 21:02:50 +0200

    Corregit esborrar línia sencera

--- EditableBufferedReader/WithMVC/Console.java
+++ EditableBufferedReader/WithMVC/Console.java
@@ -4,12 +4,37 @@ import java.util.Observer;
 public class Console implements Observer {
 
     @Override
-    public void update(Observable o, Object arg) {
-        if(arg!=null && (int) arg==Key.BELL){
-            System.out.print("\007");
+    public void update(Observable o, Object arg) { 
+        if (arg != null) {
+            switch ((int) arg) {
+                case Key.BACK:
+                    System.out.print("\033[1D");
+                    break;
+                case Key.FORW:
+                    System.out.print("\033[1C");
+                    break;
+                case Key.HOME:
+                    System.out.print("\033[1G");
+                    break;
+                case Key.END:
+                    System.out.print("\033[" + (((Line) o).getIndex() + 1) + "G");
+                    break;
+                case Key.ERASE:
+                    System.out.print("\033[1D" + "\033[1P"); // Cursor backwards, delete character
+                    break;
+                case Key.SUPR:
+                    System.out.print("\033[1P"); // Delete character
+                    break;
+                case Key.BELL:
+                    System.out.print("\007"); // Ring bell
+                case Key.CR: // Carriage return
+                    break;      
+            }          
         } else {
-            System.out.print("\033[2K" + "\033[1G" + (Line)o + "\033[" + (((Line) o).getIndex()+1) + "G"); // Erase line, cursor at begg., print line, place cursor
+            int index = ((Line) o).getIndex();
+            System.out.print("\033[0K" + ((Line) o).toString().substring(index-1) + "\033[" + (index + 1) + "G");                                                                                                    
+                
         }
     }
-    
 }
+
