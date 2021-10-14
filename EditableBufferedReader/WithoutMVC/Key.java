795cdbaadab69cedc78616ca8536093813eab815
marcgq <79192457+marcgq@users.noreply.github.com>
2021-10-14 21:08:49 +0200

    New values

--- EditableBufferedReader/WithoutMVC/Key.java
+++ EditableBufferedReader/WithoutMVC/Key.java
@@ -1,6 +1,7 @@
 public class Key {
-    public static final int CR=13, ESCAPE=27, BACKSPACE=8, BACK = 256, 
-                            FORW = 257, ERASE = 258, SUPR = 259, HOME = 260, 
-                            END = 270, INS = 271, BELL=7;
+    public static final int CR=13, ESCAPE=27, BACKSPACE=8, BELL=7, BACK = -1, 
+                            FORW = -2, ERASE = -3,  
+                            HOME = -7, INS = -6, SUPR = -5, END = -4 ;
+
 
 }
