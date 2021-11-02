eea20cdd5ccd2b65eae0dd17a089b1e42f357f11
marcgq <79192457+marcgq@users.noreply.github.com>
2021-11-02 12:17:05 +0100

    Compatible with match()
    Erase and backspace now have the same value

--- EditableBufferedReader/WithMVC_newAPIandMouse/Key.java
+++ EditableBufferedReader/WithMVC_newAPIandMouse/Key.java
@@ -1,6 +1,6 @@
 public class Key {
     public static final int CR=13, ESCAPE=27, BACKSPACE=8, BELL=7, BACK = -1, 
-                            FORW = -2, ERASE = -3,  
+                            FORW = -2, ERASE = 8,  
                             HOME = -7, INS = -6, SUPR = -5, END = -4, MOUSE=-8, ROW=-9;
 
 }
